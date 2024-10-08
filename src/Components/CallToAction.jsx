88a6c5814c686944eef8e3e118bd860cda764ccd
brayanfloresf3 <brayanfloresf3@gmail.com>
2024-10-08 00:03:50 +0000

    casi listo faltan detalles

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -1,19 +1,19 @@
 
 export function CallToAction() {
   return (
-    <div className=" inset-0 flex flex-col items-start justify-center space-y-10 relative h-5/6 w-full"
+    <div className="object-contain inset-0 flex flex-col md:items-start justify-center items-center space-y-5 md:space-y-10 relative md:h-5/6 h-full  w-full pt-40 pb-10"
       style={{
         backgroundImage: 'linear-gradient(to bottom, #202E2D, black)'
       }}>
-      <h2 className="text-6xl font-bold text-white z-10 ml-5">
-        Explore the World’s Best Cities
+      <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white z-10 ml-5">
+      Discover and Share <br />Your Adventures
       </h2>
-      <p className="text-3xl font-semibold text-gray-300 z-10 ml-5 ">
-        Discover your next adventure <br /> and see what each city has to offer.
+      <p className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-300 z-10 md:ml-5">
+      Track Your Dreams, Live Your Itineraries
       </p>
-      <img src="/assets/pexels-dico-baskoro-693731013-18070636.jpg" alt="walk" className="w-4/6 h-5/6 object-cover absolute right-0 bottom-0 rounded-l-lg  shadow-2xl shadow-custom" />
-      <a to="/cities">
-        <button type="button" class=" flex text-white font-bold text-2xl  bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-large rounded-lg px-7 py-4 pb-3text-center ml-5 mb-2 gap-2"> Visit To Cities
+      <img src="/assets/hombre-en-pueblito.jpg" alt="walk" className="w-72 md:w-4/6 h-5/6 object-cover flex justify-center items-center md:absolute md:right-0 md:bottom-0 md:rounded-l-lg  shadow-2xl shadow-custom " />
+      <a to="/cities" >
+        <button type="button" class=" flex text-white font-bold text-2xl  bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-large rounded-lg px-3 py-4 pb-3text-center ml-5 mb-2 gap-2"> Visit To Cities
 
           <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 me-2 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -20,18 +20,20 @@ export function NavBar() {
     };
 
     return (
-        <nav className={`fixed w-full z-20 top-0 start-0 transition-all duration-300 ${isScrolled ? 'bg-black bg-opacity-50' : 'bg-transparent'
+        <nav className={`fixed w-full z-20 top-0 start-0 transition-all duration-300 ${isScrolled ? 'bg-black bg-opacity-70' : 'bg-transparent'
             }`}>
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
                 <a href="#" className="flex items-center space-x-3">
-                    <span className="self-center text-2xl text-white" style={{ fontFamily: 'Bungee, cursive' }}>MY 
-                    ITINERARY</span>
-                    <span className="material-symbols-outlined text-white">
+                    <span className=" md:self-center md:text-lg md:text-2xl text-white" style={{ fontFamily: 'Bungee, cursive' }}>
+                        MY TINERARY
+                    </span>
+
+                    <span className="hidden md:inline-block material-symbols-outlined text-white text-lg">
                         travel
                     </span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 ">
-                    <button type="button" className="flex text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mt-1 mb-1 gap-2">
+                    <button type="button" className="flex text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-2 py-2 text-center mt-1 mb-1 gap-2">
                         LOGIN
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
@@ -59,16 +61,14 @@ export function NavBar() {
                 <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium md:space-x-8  md:flex-row md:mt-0">
                         <li>
-                            <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">
+                            <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0 hover:text-black ">
                                 HOME
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">CITIES</a>
+                            <a href="./cities.html" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0 hover:text-black">CITIES</a>
                         </li>
-                        <li>
-                            <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black md:p-0">SERVICIES</a>
-                        </li>
+
                     </ul>
                 </div>
             </div>
