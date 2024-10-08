5576c053da28271735b15ce30d350a0590840a1e
brayanfloresf3 <brayanfloresf3@gmail.com>
2024-10-08 00:42:03 +0000

    todo listo

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -9,11 +9,11 @@ export function CallToAction() {
       Discover and Share <br />Your Adventures
       </h2>
       <p className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-300 z-10 md:ml-5">
-      Track Your Dreams, Live Your Itineraries
+      Get inspired by the experiences of <br /> other travelers as you explore cities. <br />The adventure begins here!
       </p>
       <img src="/assets/hombre-en-pueblito.jpg" alt="walk" className="w-72 md:w-4/6 h-5/6 object-cover flex justify-center items-center md:absolute md:right-0 md:bottom-0 md:rounded-l-lg  shadow-2xl shadow-custom " />
       <a to="/cities" >
-        <button type="button" class=" flex text-white font-bold text-2xl  bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-large rounded-lg px-3 py-4 pb-3text-center ml-5 mb-2 gap-2"> Visit To Cities
+        <button type="button" class=" flex text-white font-bold text-2xl  bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-large rounded-lg px-3 py-4 pb-3text-center ml-5 mb-2 gap-2"> Visit Cities
 
           <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 me-2 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
